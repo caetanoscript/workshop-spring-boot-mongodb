379d5fd3069a62dbed8c1707152eabe192775952
caetano rech <caesteam55@gmail.com>
2025-04-15 19:53:42 -0300

    obtendo usuario por id

--- src/main/java/com/caetano/workshopmongo/service/UserService.java
+++ src/main/java/com/caetano/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.caetano.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caetano.workshopmongo.domain.User;
 import com.caetano.workshopmongo.repository.UserRepository;
+import com.caetano.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,13 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+	    Optional<User> userOptional = repo.findById(id);
+	    if (!userOptional.isPresent()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+	    return userOptional.get();  
+	}
+
 }
